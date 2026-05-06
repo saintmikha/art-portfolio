72631115c728ca78e58e6fe34c927b3596410ead
saintmikha <stmikha.art@gmail.com>
2026-05-06 22:20:00 +0800

    Delete about.js

--- about.js
+++
@@ -1,4 +0,0 @@
-// ========== ABOUT PAGE – any interactivity can go here ==========
-// Currently nothing needed, but file exists for consistency.
-// You can add dynamic content, counters, etc. later.
-console.log("About page loaded");
